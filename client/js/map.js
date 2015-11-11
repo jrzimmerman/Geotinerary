ac9b49f614a5333479b695508838a6e69039e845
Justin Zimmerman <zimmy130@gmail.com>
2015-11-11 18:20:53 -0500

    Change theme, add mapbox, add tasks   - Change theme from flatly to superhero   - Add Mapbox map   - Wireframe task "wells"

---
+++ client/js/map.js
@@ -0,0 +1,6 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoianppbW1lcm1hbiIsImEiOiJJNUp6R3lzIn0.k5J_tYDtoowIGvLx_pdwIg';
+var map = L.mapbox.map('map', 'jzimmerman.o4hd1peb');
+
+map.on('ready', function() {
+
+});
