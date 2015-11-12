27d23a5db15eda57820e36d361463ad14b8b63c3
Justin Zimmerman <zimmy130@gmail.com>
2015-11-12 08:36:23 -0500

    Fix geoloacation

--- client/js/index.js
+++ client/js/index.js
@@ -1,15 +1,22 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoianppbW1lcm1hbiIsImEiOiJJNUp6R3lzIn0.k5J_tYDtoowIGvLx_pdwIg';
 var map = L.mapbox.map('map', 'jzimmerman.o4hd1peb');
-// Set default location in case the IP doesn't have one
-  var lat = 40.8;
-  var lon = -96.67;
-  // Grab IP location from freegeoip API
-  $.getJSON('https://freegeoip.net/json/', function(json) {
-    if (json) {
-      lat = json.latitude;
-      lon = json.longitude;
-    }
-    // Build the map
-    map.setView([lat, lon], 10);
-  });
+// // Set default location in case the IP doesn't have one
+//   var lat = 40.8;
+//   var lon = -96.67;
+//   // Grab IP location from freegeoip API
+//   $.getJSON('https://freegeoip.net/json/', function(json) {
+//     if (json) {
+//       lat = json.latitude;
+//       lon = json.longitude;
+//     }
+//     // Build the map
+//     map.setView([lat, lon], 10);
+//   });
   map.featureLayer.setGeoJSON(stores);
+
+  // Geolocate with HTML 5
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      map.setView([position.coords.latitude, position.coords.longitude], 10);
+    });
+   }
