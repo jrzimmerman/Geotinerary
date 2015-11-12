f9bca8a737e7b7e972db1765f03098c37b7f606b
Justin Zimmerman <zimmy130@gmail.com>
2015-11-11 23:23:57 -0500

    fix map var scope

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,5 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoianppbW1lcm1hbiIsImEiOiJJNUp6R3lzIn0.k5J_tYDtoowIGvLx_pdwIg';
-
+var map = L.mapbox.map('map', 'jzimmerman.o4hd1peb');
 // Set default location in case the IP doesn't have one
   var lat = 40.8;
   var lon = -96.67;
@@ -10,6 +10,6 @@ L.mapbox.accessToken = 'pk.eyJ1IjoianppbW1lcm1hbiIsImEiOiJJNUp6R3lzIn0.k5J_tYDto
       lon = json.longitude;
     }
     // Build the map
-    var map = L.mapbox.map('map', 'jzimmerman.o4hd1peb').setView([lat, lon], 10);
+    map.setView([lat, lon], 10);
   });
   map.featureLayer.setGeoJSON(stores);
