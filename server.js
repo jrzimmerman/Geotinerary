2c481b2b1024846f0d8fd95ff5b48e36ac0b9a46
Justin Zimmerman <zimmy130@gmail.com>
2015-11-15 12:07:01 -0500

    remove unused geojson object

--- server.js
+++ server.js
@@ -2,12 +2,5 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-
-// Create the GeoJSON FeatureCollection of points in which we'll store
-// the additions to our itinerary.
-var points = { type: 'FeatureCollection', features: [] };
-
-
-
 app.use(express.static(path.join(__dirname, 'client')));
 app.listen(process.env.PORT || 3000);
